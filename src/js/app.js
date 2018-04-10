711ef2cc439e6e1f8809f61ff5031e75bdfa9e1b
Karol Jaworek <karoljaworek04@gmail.com>
2018-04-10 12:33:01 +0200

    done

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+$(document).on('scroll', function(){
+    let scrollTop = $(document).scrollTop();
+    if (scrollTop > 5){
+        $('#nav').addClass('-sticky');
+    }
+    else{
+        $('#nav').removeClass('-sticky');
+    }
+})
+
+$('#trigger-menu').on('click', function(){
+    $('#menu').toggleClass('-toggle');
+    $('#trigger-menu').toggleClass('-active');
+})
+
+$(document).ready(function(){
+    $('._js-anchor').on('click', function(event){
+        $('#menu').removeClass('-toggle');
+        $('#trigger-menu').removeClass('-active');
+        let target = this.hash.slice(1),
+            hash = "#",
+            targetTop = 0;
+        if(target.length){
+            hash = this.hash;
+            targetTop = $('#'+target).offset().top;
+        }
+        $('html, body').animate({ scrollTop: targetTop }, 600, history.pushState(null,null,hash));
+        event.preventDefault();
+    })
+})
